04f371b3852500ccb8d4e45d05cefe4402cb3022
Salman <salmanajani0426@gmail.com>
2022-11-30 00:21:35 +0530

    added base navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import NavBar from "./Components/Navbar";
 
 function App() {
-  return <div className="App">Under Progress</div>;
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
